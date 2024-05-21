31ecf4e7c7224f786eb3c503bbb6349a341c74ec
SaikatDev <saikat.b965@gmail.com>
2024-05-21 13:13:48 -0400

    Configure thunk

--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 // This below will help to use React Router
 import { BrowserRouter } from "react-router-dom";
 // REDUX set up----------------------------
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, applyMiddleware, compose } from "@reduxjs/toolkit";
 import rootRouter from "./reducers";
-import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 // This below is a redux connection with the app
 import { provider } from "react-redux";
+// THUNK
+import { thunk } from "redux-thunk";
+
+// We will use this if we dont need thunk
+const devToolExt =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// We need this for thunk
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // create store, SO we can use devtool and combine reducer
 const store = configureStore(
   {
     reducer: rootReducer,
   },
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancer(applyMiddleware(thunk))
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,16 +1,27 @@
+import { type } from "@testing-library/user-event/dist/type";
+
 const initState = {
   allFree: [],
   byPlatform: [],
   byId: [],
+  byCatagory: [],
 };
 
 const gamesReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
       return { ...state };
+    case "CLEAR_GAMES":
+      return {};
     default:
       return { ...state };
   }
 };
+
+// ACTION
+// {
+//   type: "FETCH_GAMES";
+// }
+// dispatch({ type: "FETCH_GAMES" });
 
 export default gamesReducer;
