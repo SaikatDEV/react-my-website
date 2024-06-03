987e24826558855cba239ca26ea87f29cf48efda
SaikatDev <saikat.b965@gmail.com>
2024-06-03 19:13:57 -0400

    Added margin

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -19,12 +19,13 @@ const AboutSection = () => {
           </div>
           <p>
             I am a Technology-minded individual with a passion for Front End Web
-            Development using REACT, VueJS, HTML, CSS, Javascript. <br /> <br />{" "}
-            As well expert on designing Test Automation Framework from scratch
-            using Selenium or any other framework like GEB, Cucumber BDD using
-            Ruby/Java, Serenity, PlayWright, Karate, Rspec, Java Junit/TestNG,
-            RestAssured API, Jmeter, QTP. As well experienced on AWS, CI-CD
-            environment using Jenkins, Bamboo, Saucelabs etc.
+            Development using REACT, VueJS, HTML, CSS, Javascript etc. <br />{" "}
+            <br /> As well expert on designing Test Automation Framework from
+            scratch using Selenium or any other framework like GEB-Spock,
+            Cucumber BDD using Ruby/Java, Serenity-BDD, PlayWright-Java, Karate,
+            Rspec, Java Junit/TestNG, RestAssured API, Jmeter, QTP. As well
+            experienced on AWS, CI-CD environment using Jenkins, Bamboo,
+            Saucelabs etc.
           </p>
           <a
             href="https://docs.google.com/document/d/1CtdfXebvplwk0VU0mjf9Q3sI3qIhjWiD/edit"
