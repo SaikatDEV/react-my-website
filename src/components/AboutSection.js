12a1ddd1cfa132830c8388d23e33fd215c89422e
SaikatDev <saikat.b965@gmail.com>
2024-06-03 22:54:39 -0400

    Refactored cert images

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -18,14 +18,19 @@ const AboutSection = () => {
             <h2>About me...</h2>
           </div>
           <p>
-            I am a Technology-minded individual with a passion for Front End Web
-            Development using REACT, VueJS, HTML, CSS, Javascript etc. <br />{" "}
-            <br /> As well expert on designing Test Automation Framework from
-            scratch using Selenium or any other framework like GEB-Spock,
-            Cucumber BDD using Ruby/Java, Serenity-BDD, PlayWright-Java, Karate,
-            Rspec, Java Junit/TestNG, RestAssured API, Jmeter, QTP. As well
-            experienced on AWS, CI-CD environment using Jenkins, Bamboo,
-            Saucelabs etc.
+            <span>
+              I am a Technology-minded individual with a passion for Front End
+              Web Development using REACT, VueJS, HTML, CSS, Javascript etc.
+            </span>
+            <br /> <br />
+            <span>
+              As well expert on designing Test Automation Framework from scratch
+              using Selenium or any other framework like GEB-Spock, Cucumber BDD
+              using Ruby/Java, Serenity-BDD, PlayWright-Java, Karate, Rspec,
+              Java Junit/TestNG, RestAssured API, Jmeter, QTP. As well
+              experienced on AWS, CI-CD environment using Jenkins, Bamboo,
+              Saucelabs etc.
+            </span>
           </p>
           <a
             href="https://docs.google.com/document/d/1CtdfXebvplwk0VU0mjf9Q3sI3qIhjWiD/edit"
